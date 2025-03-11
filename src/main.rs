38c7f4b438d25d683bc427ac03db2288a522e68c
MustacheNinja23 <michaelduffielindsay@gmail.com>
2025-03-11 13:49:53 -0400

    echo

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 use std::io::{self, Write};
 
 fn main() {
+    let stdin = io::stdin();
+    let mut input;
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
         // Wait for user input
-        let stdin = io::stdin();
-        let mut input = String::new();
+        input = String::new();
         stdin.read_line(&mut input).unwrap();
 
         let mut input_arr = input.split(" ");
 
         match input_arr.next().unwrap() {
             "exit" => break,
+            "echo" => {
+                for ele in input_arr {
+                    print!("{} ", ele);
+                }
+            }
             _ => {
                 println!("{}: command not found", input.trim());
             }
